000455890775c0e12582a82352a764e55f07501b
Agri Pramadio <119289271+agripramadio29@users.noreply.github.com>
2025-04-29 12:35:27 +0700

    push

--- store/popo/items.py
+++ store/popo/items.py
@@ -3,4 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class Item:
     image: str
+    type: str
+    name: str
     desc: str
+    price: float
--- store/views.py
+++ store/views.py
@@ -3,21 +3,32 @@ from store.popo.items import Item
 
 
 # Create your views here.
-
-def store(request):
+def get_all_items():
     items = [
-        Item(image="images/item1.jpg", desc="Harley-Davidson Pan America"),
-        Item(image="images/item2.jpg", desc="Nvidia GTX 1660 Ti"),
-        Item(image="images/item3.jpg", desc="VALORANT Prime 2.0 Phantom"),
-        Item(image="images/item4.png", desc="Kondom Fiesta Dotted"),
-        Item(image="images/item5.png", desc="New Balance 530 BKT Edition"),
-        Item(image="images/item6.jpg", desc="Vinyl Laufey Bewitched"),
+        Item(image="images/item1.jpg", type="Kendaraan" ,name="Harley-Davidson", desc="Harley-Davidson Pan America", price=230.99),
+        Item(image="images/item2.jpg", type="Komponen PC" ,name="NVIDIA", desc="Nvidia GTX 1660 Ti", price=89.99),
+        Item(image="images/item3.jpg", type="Item Game" ,name="VALORANT", desc="VALORANT Prime 2.0 Phantom", price=75.99),
+        Item(image="images/item4.png", type="Alat Sex", name="Fiesta" ,desc="Kondom Fiesta Dotted", price=20.99),
+        Item(image="images/item5.png", type="Pakaian", name="New Balance", desc="New Balance 530 BKT Edition", price=50.99),
+        Item(image="images/item6.jpg", type="Musik", name="Vinyl Record" ,desc="Vinyl Laufey Bewitched", price=20.99),
     ]
+    return items
+def store(request):
+    items = get_all_items()
     return render(request, 'store/store.html', {'items': items})
 
 
 def cart(request):
-    context = {}
+    all_items = get_all_items()
+    items = []
+    for i in range(3):
+        items.append(all_items[i])
+
+    total = sum(item.price for item in items)
+    context = {
+        'items': items,
+        'total': total,
+    }
     return render(request, 'store/cart.html', context)
 
 
